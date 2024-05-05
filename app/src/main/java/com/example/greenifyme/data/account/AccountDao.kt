3799e672bb270af00708ce7e32c9ceaf30563239
TonyGnk <61454221+TonyGnk@users.noreply.github.com>
2024-05-05 19:07:00 +0300

    Replace Java AccountDao to Kotlin and add a JavaAsyncHandler

--- app/src/main/java/com/example/greenifyme/data/account/AccountDao.kt
+++ app/src/main/java/com/example/greenifyme/data/account/AccountDao.kt
@@ -1,31 +1,50 @@
-package com.example.greenifyme.data.account;
+package com.example.greenifyme.data.account
 
-import androidx.room.Dao;
-import androidx.room.Delete;
-import androidx.room.Insert;
-import androidx.room.OnConflictStrategy;
-import androidx.room.Query;
-
-
-import java.util.List;
-
-import kotlinx.coroutines.flow.Flow;
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 
 @Dao
-public interface AccountDao {
-
+interface AccountDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    void insert(Account account);
+    suspend fun insert(account: Account)
+
+    @Update
+    suspend fun update(account: Account)
 
     @Query("DELETE FROM accounts_table")
-    void deleteAll();
+    suspend fun deleteAll()
 
-    @Query("SELECT * from accounts_table ORDER BY name ASC")
-    Flow<List<Account>> getAccounts();
+    @Query("SELECT * from accounts_table ORDER BY id ASC")
+    fun getAccounts(): Flow<List<Account>>
 
     @Query("SELECT * FROM accounts_table WHERE id = :id")
-    Flow<Account> getAccount(int id);
+    fun getAccount(id: Int): Flow<Account>
 
     @Delete
-    void delete(Account account);
+    suspend fun delete(account: Account)
+}
+
+// Async running is done in the background thread instead of the main thread
+// This class is used to handle the async operations for the database for the AccountDatabase.java
+// With that, we can call async java code from everywhere in the app
+class JavaAsyncHandler(private val accountDao: AccountDao) : ViewModel() {
+    fun insert(account: Account) {
+        viewModelScope.launch {
+            accountDao.insert(account)
+        }
+    }
+
+    fun deleteAll() {
+        viewModelScope.launch {
+            accountDao.deleteAll()
+        }
+    }
 }
