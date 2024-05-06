dc7d978f31b25947c02406435b97d8667b49c013
TonyGnk <61454221+TonyGnk@users.noreply.github.com>
2024-05-06 14:05:00 +0300

    Apply a few fixes to AdminAppBar

--- app/src/main/java/com/example/greenifyme/ui/admin/home/AdminHomeAppBar.kt
+++ app/src/main/java/com/example/greenifyme/ui/admin/home/AdminHomeAppBar.kt
@@ -3,14 +3,18 @@ package com.example.greenifyme.ui.admin.home
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Notifications
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -34,31 +38,30 @@ fun AdminHomeAppBar(
     )
 
     Row(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceBetween
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(2.dp),
+        verticalAlignment = Alignment.CenterVertically
     )
     {
-        val textmodifier = Modifier
-            .padding(start = 16.dp, top = 12.dp)
-
         Text(
             text = homeSearchUiState.greetingText,
-            modifier = textmodifier,
             style = TextStyle(
                 fontFamily = googleSansFontFamily,
                 fontWeight = FontWeight.Normal,
-                fontSize = 20.sp,
+                fontSize = 22.sp,
                 lineHeight = 24.sp,
                 letterSpacing = 0.5.sp
             ),
         )
+        Spacer(modifier = Modifier.weight(1f))
         IconButton(
             onClick = { },
             content = {
                 Icon(
-                    painterResource(
-                        id = R.drawable.baseline_notifications_24,
-                    ),
+                    //The most common icons are available in the Icons class
+                    Icons.Outlined.Notifications,
                     //This contentDescription is for accessibility
                     contentDescription = "Notifications"
                 )
