3f7371367ba6f0a6168b1756493ea4d583368f39
TonyGnk <61454221+TonyGnk@users.noreply.github.com>
2024-05-07 22:16:05 +0300

    Remove database example from select & And open Database Manager as activity

--- app/src/main/java/com/example/greenifyme/navigation/SelectTeamFragment.java
+++ app/src/main/java/com/example/greenifyme/navigation/SelectTeamFragment.java
@@ -1,6 +1,7 @@
 package com.example.greenifyme.navigation;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,9 +12,11 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
 import com.example.greenifyme.R;
+import com.example.greenifyme.data.database_example.NewWordActivity;
+import com.example.greenifyme.ui.database_manager.DBManagerActivity;
 
 public class SelectTeamFragment extends Fragment {
-
+    public static final int NEW_WORD_ACTIVITY_REQUEST_CODE = 1;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -28,7 +31,6 @@ public class SelectTeamFragment extends Fragment {
         Button buttonUser = view.findViewById(R.id.buttonUser);
         Button buttonAdmin = view.findViewById(R.id.buttonAdmin);
         Button buttonAdminCompose = view.findViewById(R.id.buttonAdminCompose);
-        Button buttonTesting = view.findViewById(R.id.buttonTesting);
         Button buttonDatabaseManager = view.findViewById(R.id.buttonDatabaseManager);
 
         // Set the onClickListeners
@@ -36,8 +38,13 @@ public class SelectTeamFragment extends Fragment {
         buttonUser.setOnClickListener(v -> Navigation.findNavController(view).navigate(R.id.action_select_team_screen_to_userHomeFragment));
         buttonAdmin.setOnClickListener(v -> Navigation.findNavController(view).navigate(R.id.action_select_team_screen_to_adminHomeFragment));
         buttonAdminCompose.setOnClickListener(v -> Navigation.findNavController(view).navigate(R.id.action_select_team_screen_to_quantityFragment));
-        buttonTesting.setOnClickListener(v -> Navigation.findNavController(view).navigate(R.id.action_select_team_screen_to_firstFragment));
-        buttonDatabaseManager.setOnClickListener(v -> Navigation.findNavController(view).navigate(R.id.action_select_team_screen_to_databaseManagerFragment));
+
+        buttonDatabaseManager.setOnClickListener(
+                v -> {
+                    Intent intent = new Intent(view.getContext(), DBManagerActivity.class);
+                    startActivityForResult(intent, NEW_WORD_ACTIVITY_REQUEST_CODE);
+                }
+        );
 
         return view;
 
