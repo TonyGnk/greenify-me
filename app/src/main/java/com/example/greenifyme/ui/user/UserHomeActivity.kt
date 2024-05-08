45bef92aad538437c73747a2b8ab0d93e4630e1b
TonyGnk <61454221+TonyGnk@users.noreply.github.com>
2024-05-09 00:10:56 +0300

    Create the foundation of Compose in user

---
+++ app/src/main/java/com/example/greenifyme/ui/user/UserHomeActivity.kt
@@ -0,0 +1,28 @@
+package com.example.greenifyme.ui.user
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import com.example.greenifyme.R
+import com.example.greenifyme.compose_utilities.theme.ComposeTheme
+import com.example.greenifyme.ui.user.home.UserHome
+
+class UserHomeActivity : AppCompatActivity() {
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.fragment_compose_user)
+        val composeView = findViewById<ComposeView>(R.id.compose_view)
+        composeView.apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                //Apply default style and colors
+                ComposeTheme {
+                    UserHome()
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/greenifyme/ui/user/home/UserHome.kt
@@ -0,0 +1,18 @@
+package com.example.greenifyme.ui.user.home
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.surfaceColorAtElevation
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.greenifyme.ui.database_manager.DBManagerNavModel
+
+@Composable
+fun UserHome() {
+
+}
