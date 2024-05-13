75575cb088a9ac9331ffee2ac835eea864efb29f
Sofia <ics21037@uom.edu.gr>
2024-05-13 16:37:48 +0300

    Added progress bar without animation to citizen points

--- app/src/main/java/com/example/greenifyme/ui/user/home/citizen_points/CitizenPoints.kt
+++ app/src/main/java/com/example/greenifyme/ui/user/home/citizen_points/CitizenPoints.kt
@@ -1,14 +1,20 @@
 package com.example.greenifyme.ui.user.home.citizen_points
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.greenifyme.R
 import com.example.greenifyme.compose_utilities.theme.ComposeTheme
 import com.example.greenifyme.ui.admin.home.shared.DefaultCard
@@ -20,25 +26,55 @@ fun CitizenPoints(
 ) {
     DefaultCard(
         horizontalPadding = dimensionResource(id = R.dimen.horizontalScreenPadding),
-        height = 250.dp
+        height = 100.dp
     ) {
-        Row(
-            horizontalArrangement = Arrangement.spacedBy(10.dp),
-            modifier = Modifier.padding(horizontal = 10.dp)
-        ) {
-            Text(
-                text = state.points.toString(),
-            )
-            Text(
-                text = state.targetPoints.toString(),
-            )
-            Text(
-                text = state.percent.toString(),
-            )
-        }
-    }
 
-}
+        Column(
+            modifier = Modifier.padding(20.dp)
+
+        )
+        {
+
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.padding(horizontal = 10.dp),
+            ) {
+                Text(
+                    text = state.points.toString() + "/",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                )
+                Text(
+                    text = state.percent.toString() + "%",
+                    fontSize = 10.sp,
+                    modifier = Modifier.padding(horizontal = 15.dp)
+                        
+                )
+
+
+
+            }
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(30.dp),
+                    modifier = Modifier.padding(horizontal = 10.dp),
+                ) {
+                    Text(
+                        text = state.targetPoints.toString(),
+                    )
+                    LinearProgressIndicator(
+                        modifier = Modifier.width(200.dp).height(7.dp),
+                        progress = {
+                            state.percent/100
+                        },
+
+                    )
+                }
+
+
+            }
+        }
+
+    }
 
 
 @Preview
