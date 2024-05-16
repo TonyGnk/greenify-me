0fe1da64fd33932c6d13276992106c20790a8b62
TonyGnk <61454221+TonyGnk@users.noreply.github.com>
2024-05-16 22:21:12 +0300

    Add 5 million database functions

--- app/src/main/java/com/example/greenifyme/data/GreenDao.kt
+++ app/src/main/java/com/example/greenifyme/data/GreenDao.kt
@@ -1,223 +1,287 @@
 package com.example.greenifyme.data
 
-import androidx.room.Dao
-import androidx.room.Delete
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
-import androidx.room.Transaction
-import androidx.room.Update
-import com.example.greenifyme.data.account.hashPassword
-import com.example.greenifyme.data.account.initialAccounts
-import com.example.greenifyme.data.material.initialMaterials
-import com.example.greenifyme.data.form.initialForms
-import com.example.greenifyme.data.relations.AccountWithRecords
-import com.example.greenifyme.data.relations.FormMaterialCrossRef
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.launch
+import androidx.room.*
+import com.example.greenifyme.data.account.*
+import com.example.greenifyme.data.form.*
+import com.example.greenifyme.data.material.*
+import com.example.greenifyme.data.relations.*
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
 
 @Dao
 interface GreenDao {
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertAccount(account: Account)
+	@Insert(onConflict = OnConflictStrategy.REPLACE)
+	suspend fun insertAccount(account : Account)
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertRecord(form: Form)
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertEntry(material: Material)
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertRecordMaterialCrossRef(crossRef: FormMaterialCrossRef)
+	@Insert(onConflict = OnConflictStrategy.REPLACE)
+	suspend fun insertForm(form : Form)
 
-    //______________________________________________
 
-    @Update
-    suspend fun update(account: Account)
 
-    @Update
-    suspend fun update(form: Form)
+	@Insert(onConflict = OnConflictStrategy.REPLACE)
+	suspend fun insertEntry(material : Material)
 
-    @Update
-    suspend fun update(material: Material)
 
-    //______________________________________________
 
-    @Query("SELECT * FROM accounts_table WHERE email = :email")
-    fun accountExists(email: String): Boolean
+	@Insert(onConflict = OnConflictStrategy.REPLACE)
+	suspend fun insertFormMaterialCrossRef(crossRef : FormMaterialCrossRef)
 
-    @Query("SELECT * FROM accounts_table WHERE email = :email AND password = :hash")
-    fun accountExists(email: String, hash: String): Boolean
+	//______________________________________________
 
-    //______________________________________________
+	@Update
+	suspend fun update(account : Account)
 
-    @Query("SELECT * FROM accounts_table WHERE id = :id")
-    fun getAccount(id: Int): Flow<Account>
 
-    @Query("SELECT * from accounts_table ORDER BY id ASC")
-    fun getAccounts(): Flow<List<Account>>
 
-    @Query("SELECT * FROM forms_table WHERE formId = :id")
-    fun getRecord(id: Int): Flow<Form>
+	@Update
+	suspend fun update(form : Form)
 
-    @Query("SELECT * from forms_table ORDER BY formId ASC")
-    fun getRecords(): Flow<List<Form>>
 
-    @Query("SELECT * FROM materials_table WHERE materialId = :id")
-    fun getMaterial(id: Int): Flow<Material>
 
-    @Query("SELECT * from materials_table ORDER BY materialId ASC")
-    fun getMaterials(): Flow<List<Material>>
+	@Update
+	suspend fun update(material : Material)
 
-    //
-    @Transaction
-    @Query(
-        """
+	//______________________________________________
+
+	@Query("SELECT * FROM accounts_table WHERE email = :email")
+	fun accountExists(email : String) : Boolean
+
+
+
+	@Query("SELECT * FROM accounts_table WHERE email = :email AND password = :hash")
+	fun accountExists(email : String, hash : String) : Boolean
+
+	//______________________________________________
+
+	@Query("SELECT * FROM accounts_table WHERE id = :id")
+	fun getAccount(id : Int) : Flow<Account>
+
+
+
+	@Query("SELECT * from accounts_table ORDER BY id ASC")
+	fun getAccounts() : Flow<List<Account>>
+
+
+
+	@Query("SELECT * FROM forms_table WHERE formId = :id")
+	fun getForm(id : Int) : Flow<Form>
+
+
+
+	@Query("SELECT * from forms_table ORDER BY formId ASC")
+	fun getForms() : Flow<List<Form>>
+
+
+
+	// getFormLatestIndex
+	@Query("SELECT MAX(formId) FROM forms_table")
+	fun getFormLatestIndex() : Flow<Int>
+
+
+
+	@Query("SELECT * FROM materials_table WHERE materialId = :id")
+	fun getMaterial(id : Int) : Flow<Material>
+
+
+
+	@Query("SELECT * from materials_table ORDER BY materialId ASC")
+	fun getMaterials() : Flow<List<Material>>
+
+
+
+	@Query("SELECT * FROM materials_table WHERE category = :category ORDER BY materialId ASC")
+	fun getMaterialsWith(category : RecyclingCategory) : Flow<List<Material>>
+
+
+
+	//
+	@Transaction
+	@Query(
+		"""
     SELECT m.name AS materialName
     FROM materials_table AS m
     INNER JOIN form_material_cross_ref AS c ON m.materialId = c.materialId
     GROUP BY m.materialId
 """
-    )
-    fun getMaterialNames(): Flow<List<String>>
-
-    @Transaction
-    @Query("SELECT * FROM accounts_table WHERE id = :accountId")
-    fun getAccountWithRecords(accountId: Int): List<AccountWithRecords>
-
-    @Transaction
-    @Query("SELECT SUM(quantity) FROM form_material_cross_ref WHERE materialId = :materialId ORDER BY materialId ASC")
-    fun getQuantityForMaterial(materialId: Int): Flow<Int>
-
-    @Query("SELECT SUM(quantity) AS quantity FROM form_material_cross_ref GROUP BY materialId")
-    fun getQuantityForAllMaterials(): Flow<List<Int>>
+	)
+	fun getMaterialNames() : Flow<List<String>>
 
 
-    //______________________________________________
 
-    @Delete
-    suspend fun deleteAccount(account: Account)
+	@Transaction
+	@Query("SELECT * FROM accounts_table WHERE id = :accountId")
+	fun getAccountWithForms(accountId : Int) : List<AccountWithForm>
 
-    @Query("DELETE FROM accounts_table")
-    suspend fun deleteAccounts()
 
-    @Delete
-    suspend fun deleteRecord(form: Form)
 
-    @Query("DELETE FROM forms_table")
-    suspend fun deleteRecords()
+	@Transaction
+	@Query("SELECT SUM(quantity) FROM form_material_cross_ref WHERE materialId = :materialId ORDER BY materialId ASC")
+	fun getQuantityForMaterial(materialId : Int) : Flow<Int>
 
-    @Delete
-    suspend fun deleteMaterial(material: Material)
 
-    @Query("DELETE FROM materials_table")
-    suspend fun deleteMaterials()
 
-    @Query("DELETE FROM forms_table WHERE accountId = :accountId")
-    suspend fun deleteRecordsByAccountId(accountId: Int)
+	@Query("SELECT SUM(quantity) AS quantity FROM form_material_cross_ref GROUP BY materialId")
+	fun getQuantityForAllMaterials() : Flow<List<Int>>
+
+	//______________________________________________
+
+	@Delete
+	suspend fun deleteAccount(account : Account)
+
+
+
+	@Query("DELETE FROM accounts_table WHERE id = :id")
+	suspend fun deleteAccount(id : Int)
+
+
+
+	@Query("DELETE FROM accounts_table")
+	suspend fun deleteAccounts()
+
+
+
+	@Delete
+	suspend fun deleteForm(form : Form)
+
+
+
+	@Query("DELETE FROM forms_table WHERE formId = :id")
+	suspend fun deleteForm(id : Int)
+
+
+
+	@Query("DELETE FROM forms_table")
+	suspend fun deleteForms()
+
+
+
+	@Delete
+	suspend fun deleteMaterial(material : Material)
+
+
+
+	@Query("DELETE FROM materials_table WHERE materialId = :id")
+	suspend fun deleteMaterial(id : Int)
+
+
+
+	@Query("DELETE FROM materials_table")
+	suspend fun deleteMaterials()
+
+
+
+	@Query("DELETE FROM forms_table WHERE accountId = :accountId")
+	suspend fun deleteFormsByAccountId(accountId : Int)
 }
 
-class GreenRepository(private val dao: GreenDao) {
-    fun init(type: DataObjectType, scope: CoroutineScope) {
-        when (type) {
-            DataObjectType.ACCOUNT -> initialAccounts
-            DataObjectType.RECORD -> initialForms
-            DataObjectType.MATERIAL -> initialMaterials
-        }.forEach { insert(it, scope) }
+class GreenRepository(private val dao : GreenDao) {
+	fun init(type : DataObjectType, scope : CoroutineScope) {
+		when (type) {
+			DataObjectType.ACCOUNT -> initialAccounts
+			DataObjectType.FORM -> initialForms
+			DataObjectType.MATERIAL -> initialMaterials
+		}.forEach { insert(it, scope) }
 
-        //Add relationships cross
-        val crossRefs = listOf(
-            FormMaterialCrossRef(1, 1, 1),
-            FormMaterialCrossRef(2, 2, 5),
-            FormMaterialCrossRef(3, 3, 30),
-            FormMaterialCrossRef(4, 4, 1),
-            FormMaterialCrossRef(5, 5, 11),
-            FormMaterialCrossRef(6, 3, 1),
-            FormMaterialCrossRef(7, 4, 4),
-            FormMaterialCrossRef(8, 2, 6),
-            FormMaterialCrossRef(9, 1, 2),
-            FormMaterialCrossRef(10, 5, 1),
-            FormMaterialCrossRef(11, 6, 22),
-            FormMaterialCrossRef(12, 9, 2),
-            FormMaterialCrossRef(13, 8, 1),
-            FormMaterialCrossRef(14, 7, 22),
-        )
-        crossRefs.forEach { insertCrossRef(it, scope) }
-    }
+		// Add relationships cross
+		val crossRefs = listOf(
+			FormMaterialCrossRef(1, 1, 1),
+			FormMaterialCrossRef(1, 2, 5),
+			FormMaterialCrossRef(1, 3, 30),
+			FormMaterialCrossRef(1, 4, 1),
+			FormMaterialCrossRef(5, 5, 11),
+			FormMaterialCrossRef(6, 3, 1),
+			FormMaterialCrossRef(7, 4, 4),
+			FormMaterialCrossRef(8, 2, 6),
+			FormMaterialCrossRef(9, 1, 2),
+			FormMaterialCrossRef(10, 5, 1),
+			FormMaterialCrossRef(11, 6, 22),
+			FormMaterialCrossRef(12, 9, 2),
+			FormMaterialCrossRef(13, 8, 1),
+			FormMaterialCrossRef(14, 7, 22),
+		)
+		crossRefs.forEach { insertCrossRef(it, scope) }
+	}
 
-    fun insert(item: DataObject, scope: CoroutineScope) =
-        scope.launch {
-            when (item) {
-                is Account -> dao.insertAccount(item)
-                is Form -> dao.insertRecord(item)
-                is Material -> dao.insertEntry(item)
-            }
-        }
+	fun insert(item : DataObject, scope : CoroutineScope) =
+		scope.launch {
+			when (item) {
+				is Account -> dao.insertAccount(item)
+				is Form -> dao.insertForm(item)
+				is Material -> dao.insertEntry(item)
+			}
+		}
 
-    fun insertCrossRef(crossRef: FormMaterialCrossRef, scope: CoroutineScope) =
-        scope.launch {
-            dao.insertRecordMaterialCrossRef(crossRef)
-        }
+	fun insertCrossRef(crossRef : FormMaterialCrossRef, scope : CoroutineScope) =
+		scope.launch {
+			dao.insertFormMaterialCrossRef(crossRef)
+		}
 
-    fun update(item: DataObject, scope: CoroutineScope) = scope.launch {
-        when (item) {
-            is Account -> dao.update(item)
-            is Form -> dao.update(item)
-            is Material -> dao.update(item)
-        }
-    }
+	fun update(item : DataObject, scope : CoroutineScope) = scope.launch {
+		when (item) {
+			is Account -> dao.update(item)
+			is Form -> dao.update(item)
+			is Material -> dao.update(item)
+		}
+	}
 
-    fun accountExists(email: String): Boolean = dao.accountExists(email)
+	fun accountExists(email : String) : Boolean = dao.accountExists(email)
+	fun accountExists(email : String, password : String) : Boolean =
+		dao.accountExists(email, hashPassword(password))
 
-    fun accountExists(email: String, password: String): Boolean =
-        dao.accountExists(email, hashPassword(password))
+	fun getAccount(id : Int) : Flow<Account?> = dao.getAccount(id)
+	fun getAccounts() : Flow<List<Account>> = dao.getAccounts()
+	fun getForm(id : Int) : Flow<Form?> = dao.getForm(id)
+	fun getForms() : Flow<List<Form>> = dao.getForms()
+	fun getFormLatestIndex() : Flow<Int> = dao.getFormLatestIndex()
+	fun getMaterial(id : Int) : Flow<Material?> = dao.getMaterial(id)
+	fun getMaterials() : Flow<List<Material>> = dao.getMaterials()
+	fun getMaterialsWith(category : RecyclingCategory) : Flow<List<Material>> =
+		dao.getMaterialsWith(category)
 
-    fun getAccount(id: Int): Flow<Account?> = dao.getAccount(id)
-    fun getAccounts(): Flow<List<Account>> = dao.getAccounts()
+	fun getMaterialNames() : Flow<List<String>> = dao.getMaterialNames()
+	fun getSumsForMaterial(materialId : Int) : Flow<Int> = dao.getQuantityForMaterial(materialId)
+	fun getQuantityForAllMaterials() : Flow<List<Int>> = dao.getQuantityForAllMaterials()
+	fun deleteWithId(type : DataObjectType, id : Int, scope : CoroutineScope) =
+		scope.launch {
+			when (type) {
+				DataObjectType.ACCOUNT -> dao.deleteAccount(id)
+				DataObjectType.FORM -> dao.deleteForm(id)
+				DataObjectType.MATERIAL -> dao.deleteMaterial(id)
+			}
+		}
 
-    fun getRecord(id: Int): Flow<Form?> = dao.getRecord(id)
-    fun getRecords(): Flow<List<Form>> = dao.getRecords()
+	fun delete(item : DataObject, scope : CoroutineScope) = scope.launch {
+		when (item) {
+			is Account -> {
+				dao.deleteFormsByAccountId(item.id)
+				dao.deleteAccount(item)
+			}
 
-    fun getMaterial(id: Int): Flow<Material?> = dao.getMaterial(id)
-    fun getMaterials(): Flow<List<Material>> = dao.getMaterials()
+			is Form -> dao.deleteForm(item)
+			is Material -> dao.deleteMaterial(item)
+		}
+	}
 
-    fun getMaterialNames(): Flow<List<String>> = dao.getMaterialNames()
+	fun deleteAll(type : DataObjectType, scope : CoroutineScope, alsoPopulate : Boolean = false) =
+		scope.launch {
+			when (type) {
+				DataObjectType.ACCOUNT -> {
+					dao.deleteAccounts()
+					if (alsoPopulate) init(type, scope)
+				}
 
-    fun getSumsForMaterial(materialId: Int): Flow<Int> = dao.getQuantityForMaterial(materialId)
+				DataObjectType.FORM -> {
+					dao.deleteForms()
+					if (alsoPopulate) init(type, scope)
+				}
 
-    fun getQuantityForAllMaterials(): Flow<List<Int>> = dao.getQuantityForAllMaterials()
-
-    fun delete(item: DataObject, scope: CoroutineScope) = scope.launch {
-        when (item) {
-            is Account -> {
-                dao.deleteRecordsByAccountId(item.id)
-                dao.deleteAccount(item)
-            }
-
-            is Form -> dao.deleteRecord(item)
-            is Material -> dao.deleteMaterial(item)
-        }
-    }
-
-    fun deleteAll(type: DataObjectType, scope: CoroutineScope, alsoPopulate: Boolean = false) =
-        scope.launch {
-            when (type) {
-                DataObjectType.ACCOUNT -> {
-                    dao.deleteAccounts()
-                    if (alsoPopulate) init(type, scope)
-                }
-
-                DataObjectType.RECORD -> {
-                    dao.deleteRecords()
-                    if (alsoPopulate) init(type, scope)
-                }
-
-                DataObjectType.MATERIAL -> {
-                    dao.deleteMaterials()
-                    if (alsoPopulate) init(type, scope)
-                }
-            }
-        }
+				DataObjectType.MATERIAL -> {
+					dao.deleteMaterials()
+					if (alsoPopulate) init(type, scope)
+				}
+			}
+		}
 }
--- app/src/main/java/com/example/greenifyme/ui/database_manager/content_shared/model/ContentViewModel.kt
+++ app/src/main/java/com/example/greenifyme/ui/database_manager/content_shared/model/ContentViewModel.kt
@@ -1,172 +1,160 @@
 package com.example.greenifyme.ui.database_manager.content_shared.model
 
-import com.example.greenifyme.data.Account
-import com.example.greenifyme.data.DataObject
-import com.example.greenifyme.data.DataObjectType
-import com.example.greenifyme.data.GreenRepository
-import com.example.greenifyme.data.Material
-import com.example.greenifyme.data.Form
-import com.example.greenifyme.ui.database_manager.DBManagerNavDestination
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.update
+import android.content.*
+import com.example.greenifyme.data.*
+import com.example.greenifyme.ui.database_manager.*
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
 
 interface ContentViewModel {
-    val databaseItems: StateFlow<List<DataObject>>
-    val uiState: MutableStateFlow<ContentUiState>
-    val scope: CoroutineScope
-    val repository: GreenRepository
-    val destination: DBManagerNavDestination
+	val databaseItems : StateFlow<List<DataObject>>
+	val uiState : MutableStateFlow<ContentUiState>
+	val scope : CoroutineScope
+	val repository : GreenRepository
+	val destination : DBManagerNavDestination
+	fun saveItem() {
+		if (validateInput()) {
+			repository.insert(
+				when (val item = uiState.value.itemState) {
+					is AccountState -> item.toAccount()
+					is RecordState -> item.toRecord()
+					is MaterialState -> item.toMaterial()
+				}, scope
+			)
+		}
+	}
 
-    fun saveItem() {
-        if (validateInput()) {
-            repository.insert(
-                when (val item = uiState.value.itemState) {
-                    is AccountState -> item.toAccount()
-                    is RecordState -> item.toRecord()
-                    is MaterialState -> item.toMaterial()
-                }, scope
-            )
-        }
-    }
+	fun updateItem() {
+		if (validateInput()) {
+			repository.update(
+				when (val item = uiState.value.itemState) {
+					is AccountState -> item.toAccount()
+					is RecordState -> item.toRecord()
+					is MaterialState -> item.toMaterial()
+				}, scope
+			)
+		}
+	}
 
-    fun updateItem() {
-        if (validateInput()) {
-            repository.update(
-                when (val item = uiState.value.itemState) {
-                    is AccountState -> item.toAccount()
-                    is RecordState -> item.toRecord()
-                    is MaterialState -> item.toMaterial()
-                }, scope
-            )
-        }
+	fun deleteItem(item : DataObject) = repository.delete(item, scope)
+	fun setBottomSheet(value : Boolean) {
+		uiState.update {
+			it.copy(showBottomSheet = value)
+		}
+	}
 
-    }
+	fun openSheetAndSwitchToCreateMode() {
+		uiState.update {
+			it.copy(
+				itemState =
+				when (destination) {
+					DBManagerNavDestination.Account -> AccountState()
+					DBManagerNavDestination.Record -> RecordState()
+					DBManagerNavDestination.Material -> MaterialState()
+				},
+				openSheetForEditing = false,
+				showBottomSheet = true,
+				isEntryValid = true
+			)
+		}
+	}
 
-    fun deleteItem(item: DataObject) = repository.delete(item, scope)
+	fun validateInput(changedUiState : ItemState = uiState.value.itemState) : Boolean {
+		return when (changedUiState) {
+			is AccountState -> with(changedUiState) {
+				name.isNotBlank() && email.isNotBlank() && password.isNotBlank()
+			}
 
+			is RecordState -> true
+			is MaterialState -> true
+		}
+	}
 
-    fun setBottomSheet(value: Boolean) {
-        uiState.update {
-            it.copy(showBottomSheet = value)
-        }
-    }
+	fun searchOnSearch(closeAlso : Boolean = false) {
+		if (uiState.value.searchQuery.isNotEmpty() && !closeAlso)
+			uiState.update { it.copy(searchQuery = "") }
+		else
+			uiState.update { it.copy(isSearching = false, searchQuery = "") }
+	}
 
-    fun openSheetAndSwitchToCreateMode() {
-        uiState.update {
-            it.copy(
-                itemState =
-                when (destination) {
-                    DBManagerNavDestination.Account -> AccountState()
-                    DBManagerNavDestination.Record -> RecordState()
-                    DBManagerNavDestination.Material -> MaterialState()
-                },
-                openSheetForEditing = false,
-                showBottomSheet = true,
-                isEntryValid = true
-            )
-        }
-    }
+	fun searchOnQueryChange(query : String) {
+		uiState.update {
+			it.copy(
+				searchQuery = query,
+				isSearching = if (query.isNotEmpty()) true else it.isSearching
+			)
+		}
+	}
 
+	fun updateFields(uiFields : ItemState) {
+		uiState.update {
+			it.copy(
+				itemState = uiFields,
+				isEntryValid = validateInput(uiFields)
+			)
+		}
+	}
 
-    fun validateInput(changedUiState: ItemState = uiState.value.itemState): Boolean {
-        return when (changedUiState) {
-            is AccountState -> with(changedUiState) {
-                name.isNotBlank() && email.isNotBlank() && password.isNotBlank()
-            }
+	fun deleteAll(alsoPopulate : Boolean = false) {
+		repository.deleteAll(
+			when (uiState.value.itemState) {
+				is AccountState -> DataObjectType.ACCOUNT
+				is RecordState -> DataObjectType.FORM
+				is MaterialState -> DataObjectType.MATERIAL
+			}, scope, alsoPopulate
+		)
+	}
 
-            is RecordState -> true
-            is MaterialState -> true
-        }
+	fun onListItemClick(item : DataObject) {
+		uiState.update {
+			it.copy(
+				selectedAccount = item,
+				itemState = when (item) {
+					is Account -> item.toAccountState()
+					is Form -> item.toRecordState()
+					is Material -> item.toMaterialState()
+				},
+				isEntryValid = true,
+				openSheetForEditing = true,
+				showBottomSheet = true
+			)
+		}
+	}
 
-    }
+	fun searchOnSearchButton() {
+		uiState.update {
+			it.copy(
+				searchQuery = "",
+				isSearching = false
+			)
+		}
+	}
 
-    fun searchOnSearch(closeAlso: Boolean = false) {
-        if (uiState.value.searchQuery.isNotEmpty() && !closeAlso)
-            uiState.update { it.copy(searchQuery = "") }
-        else
-            uiState.update { it.copy(isSearching = false, searchQuery = "") }
-    }
-
-    fun searchOnQueryChange(query: String) {
-        uiState.update {
-            it.copy(
-                searchQuery = query,
-                isSearching = if (query.isNotEmpty()) true else it.isSearching
-            )
-        }
-    }
-
-
-    fun updateFields(uiFields: ItemState) {
-        uiState.update {
-            it.copy(
-                itemState = uiFields,
-                isEntryValid = validateInput(uiFields)
-            )
-        }
-    }
-
-    fun deleteAll(alsoPopulate: Boolean = false) {
-        repository.deleteAll(
-            when (uiState.value.itemState) {
-                is AccountState -> DataObjectType.ACCOUNT
-                is RecordState -> DataObjectType.RECORD
-                is MaterialState -> DataObjectType.MATERIAL
-            }, scope, alsoPopulate
-        )
-    }
-
-
-    fun onListItemClick(item: DataObject) {
-        uiState.update {
-            it.copy(
-                selectedAccount = item,
-                itemState = when (item) {
-                    is Account -> item.toAccountState()
-                    is Form -> item.toRecordState()
-                    is Material -> item.toMaterialState()
-                },
-                isEntryValid = true,
-                openSheetForEditing = true,
-                showBottomSheet = true
-            )
-        }
-    }
-
-    fun searchOnSearchButton() {
-        uiState.update {
-            it.copy(
-                searchQuery = "",
-                isSearching = false
-            )
-        }
-    }
-
-    fun searchGetResults(): List<DataObject> {
-        val searchQuery = uiState.value.searchQuery
-        return if (searchQuery.isEmpty()) listOf()
-        else if (searchQuery.toIntOrNull() != null)
-            databaseItems.value.filter {
-                when (it) {
-                    is Account -> it.id == searchQuery.toInt()
-                    is Form -> it.formId == searchQuery.toInt() || it.accountId == searchQuery.toInt()
-                    is Material -> it.materialId == searchQuery.toInt()
-                }
-            }
-        else
-            databaseItems.value.filter {
-                when (it) {
-                    is Account -> it.name.contains(searchQuery, ignoreCase = true)
-                    is Form -> false
-                    is Material -> it.name.contains(
-                        searchQuery,
-                        ignoreCase = true
-                    ) || it.category.description.contains(searchQuery, ignoreCase = true)
-                }
-            }
-    }
+	fun searchGetResults(context : Context) : List<DataObject> {
+		val searchQuery = uiState.value.searchQuery
+		return if (searchQuery.isEmpty()) listOf()
+		else if (searchQuery.toIntOrNull() != null)
+			databaseItems.value.filter {
+				when (it) {
+					is Account -> it.id == searchQuery.toInt()
+					is Form -> it.formId == searchQuery.toInt() || it.accountId == searchQuery.toInt()
+					is Material -> it.materialId == searchQuery.toInt()
+				}
+			}
+		else
+			databaseItems.value.filter {
+				when (it) {
+					is Account -> it.name.contains(searchQuery, ignoreCase = true)
+					is Form -> false
+					is Material -> it.name.contains(
+						searchQuery,
+						ignoreCase = true
+					) || context
+						.getString(it.category.description)
+						.contains(searchQuery, ignoreCase = true)
+				}
+			}
+	}
 }
 
 
