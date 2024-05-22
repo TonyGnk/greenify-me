10e2d9d54f3d6b372ebe37510c8184fc0973b772
Miltiades Papadopoulos <miltospap5@gmail.com>
2024-05-22 18:47:54 +0300

    Login functionality #2
     - Added password validation (both not null and check in the DB with the password and email)
     - Added some id's i forgot about before in the fragment_password.xml 's

--- app/src/main/java/com/example/greenifyme/ui/login/PasswordViewModel.java
+++ app/src/main/java/com/example/greenifyme/ui/login/PasswordViewModel.java
@@ -1,7 +1,24 @@
 package com.example.greenifyme.ui.login;
 
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.ViewModel;
 
-public class PasswordViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+import com.example.greenifyme.ApplicationSetup;
+import com.example.greenifyme.data.GreenRepository;
+
+public class PasswordViewModel extends AndroidViewModel {
+    private GreenRepository mRepository;
+
+    public PasswordViewModel(Application application) {
+        super(application);
+        ApplicationSetup app = (ApplicationSetup) application;
+        mRepository = app.greenRepository;
+
+    }
+    public boolean isCredentialsCorrect(String email, String hash) {
+        return mRepository.accountExists(email, hash);
+    }
+
 }
