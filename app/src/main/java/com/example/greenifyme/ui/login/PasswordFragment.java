fada80a6fbfee68f29ee9dd97a21e5eb72d968ea
TonyGnk <61454221+TonyGnk@users.noreply.github.com>
2024-05-30 01:32:09 +0300

    Refactoring Login Functionality for Type Safety, optimization, and enforce clean architecture

--- app/src/main/java/com/example/greenifyme/ui/login/PasswordFragment.java
+++ app/src/main/java/com/example/greenifyme/ui/login/PasswordFragment.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,59 +24,78 @@ import java.util.Objects;
 
 public class PasswordFragment extends Fragment {
 
-    private PasswordViewModel mViewModel;
+    private RegisterViewModel model;
+
     private String password;
+
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
-    public static PasswordFragment newInstance() {
-        return new PasswordFragment();
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        model = new ViewModelProvider(this).get(RegisterViewModel.class);
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState
+    ) {
         //Inflate layout
-        View view = inflater.inflate(R.layout.fragment_password, container, false);
+        View view = inflater.inflate(
+                R.layout.fragment_password,
+                container,
+                false
+        );
 
         //Getting components
-        Button buttonForgotPassword = view.findViewById(R.id.forgotPasswordButton);
+        Button buttonForgotPassword =
+                view.findViewById(R.id.forgotPasswordButton);
         TextView emailPreviewText = view.findViewById(R.id.emailPreviewText);
-        TextInputEditText txtPassword = view.findViewById(R.id.passwordEditTextLogin);
+        TextInputEditText txtPassword =
+                view.findViewById(R.id.passwordEditTextLogin);
         Button buttonSignInLogin = view.findViewById(R.id.buttonSignInLogin);
 
         //Getting email from Login email
-        SharedPreferences sp = requireContext().getSharedPreferences("myUserPrefs", Context.MODE_PRIVATE);
+        SharedPreferences sp = requireContext().getSharedPreferences(
+                "myUserPrefs",
+                Context.MODE_PRIVATE
+        );
         //will be used as a parameter to authenticate account
         String email = sp.getString("email", "");
         emailPreviewText.setText(email);
 
-        //submit button functionality
         buttonSignInLogin.setOnClickListener(v -> {
+            model.sendPasswordFieldToModel(
+                    Objects.requireNonNull(txtPassword.getText()).toString()
+            );
+            model.sendEmailFieldToModel(email);
 
-            setPassword(Objects.requireNonNull(txtPassword.getText()).toString()); //Storing email to pass to isEmailRegistered()
-
-            //Checks if passwordField is not null and if it matches with the email in the DataBase
-            if (!TextUtils.isEmpty(getPassword()) &&  mViewModel.isCredentialsCorrect(email, getPassword())) {
-                //TODO: Need to hide login fragments after this redirect
-                Intent intent = new Intent(view.getContext(), UserHomeActivity.class);
-                view.getContext().startActivity(intent);
-            } else {
-                txtPassword.setError("The password is not correct!, Try again!");
-            }
+            model.onSignInPressed(result -> {
+                if (Boolean.TRUE.equals(result)) {
+                    Intent intent = new Intent(
+                            view.getContext(),
+                            UserHomeActivity.class
+                    );
+                    view.getContext().startActivity(intent);
+                } else {
+                    txtPassword.setError("The password is not correct!, Try " +
+                            "again!");
+                }
+            });
         });
 
-        buttonForgotPassword.setOnClickListener(v -> Toast.makeText(getContext(), "Forgot Password Not implemented!", Toast.LENGTH_LONG).show());
+        buttonForgotPassword.setOnClickListener(v -> Toast.makeText(
+                getContext(),
+                "Forgot Password Not implemented!",
+                Toast.LENGTH_LONG
+        ).show());
         return view;
     }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(PasswordViewModel.class);
-    }
-
 }
