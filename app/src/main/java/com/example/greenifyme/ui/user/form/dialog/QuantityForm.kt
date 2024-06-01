e61075e39bd172222eb990642fc28c7000051ee3
TonyGnk <61454221+TonyGnk@users.noreply.github.com>
2024-06-01 21:40:52 +0300

    A small refresh to the original example in QuantityForm

--- app/src/main/java/com/example/greenifyme/ui/user/form/dialog/QuantityForm.kt
+++ app/src/main/java/com/example/greenifyme/ui/user/form/dialog/QuantityForm.kt
@@ -7,6 +7,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import com.example.greenifyme.data.Both
+import com.example.greenifyme.data.Grams
 import com.example.greenifyme.data.Material
 import com.example.greenifyme.data.OptionsType
 import com.example.greenifyme.data.Pieces
@@ -15,26 +17,22 @@ import com.example.greenifyme.ui.user.form.UserFormState
 
 @Composable
 fun QuantityForm(model: UserFormModel, state: UserFormState) {
-    val selectedMaterial: Material = state.selectedMaterial
 
-    // Possible options WEIGHT, PIECE, PIECES_WEIGHT
     val typeOfOptions: OptionsType = Pieces(23)// selectedMaterial.options.type
 
-    // If the type is WEIGHT this will be null
-    val pointsPerPiece: Int = 23 //selectedMaterial.options.pointsPerPiece
-    // If the type is PIECE this will be null
-    val pointsPerGram: Int = 23//selectedMaterial.options.pointsPerGram
+    val pointsPerPiece = 23
+    val pointsPerGram = 23
 
     val textFieldContent = remember { mutableStateOf("") }
 
     Column {
-//        if (typeOfOptions == OptionsType.WEIGHT && pointsPerGram != null) {
-//            Text(text = "Weight Options Available with points per gram: $pointsPerGram")
-//        } else if (typeOfOptions == OptionsType.PIECES && pointsPerPiece != null) {
-//            Text(text = "Pieces Options Available with points per piece: $pointsPerPiece")
-//        } else if (typeOfOptions == OptionsType.PIECES_WEIGHT && pointsPerPiece != null && pointsPerGram != null) {
-//            Text(text = "All Options Available with points per piece: $pointsPerPiece and points per gram: $pointsPerGram")
-//        }
+        if (typeOfOptions is Grams) {
+            Text(text = "Weight Options Available with points per gram: $pointsPerGram")
+        } else if (typeOfOptions is Grams) {
+            Text(text = "Pieces Options Available with points per piece: $pointsPerPiece")
+        } else if (typeOfOptions is Both) {
+            Text(text = "All Options Available with points per piece: $pointsPerPiece and points per gram: $pointsPerGram")
+        }
 
         OutlinedTextField(
             value = textFieldContent.value,
